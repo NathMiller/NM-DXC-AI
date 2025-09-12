c473e4ff85c8d1c5c61bc5eb04faf54f50dda68b
NathMiller <nathanmiller5304@gmail.com>
2025-09-12 19:05:17 +0100

    Added class that will be how the AI tackles the goal of improving email

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#goals of the ai 
+class EmailRequest(BaseModel):
+    text: str
+    tone: str = "Professional"
+    goals: str = "Make it clear, concise, and well articulated"
+
 @app.get("/improve")
 def improveEmail():
     response: ChatResponse = chat(model='llama3', messages=[{'role': 'user', 'content': "Ten restaurants to visit in Newcastle? Names only"}])
