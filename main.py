60c0bbc92cbba098cf10d2d6950ffe116fa00f89
NathMiller <nathanmiller5304@gmail.com>
2025-09-12 19:34:18 +0100

    Added CORS to allow vite dev server access to FastAPI

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ollama import chat, ChatResponse
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+
 
 #goals of the ai 
 class EmailRequest(BaseModel):
