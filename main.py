c9af3804334a97bf7da52f05895c6891ab0bf507
NathMiller <nathanmiller5304@gmail.com>
2025-09-12 19:04:05 +0100

    Initial request to AI model for testing. Runs on localhost and does not take parameters yet

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
 from ollama import chat, ChatResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get("/improve")
+def improveEmail():
+    response: ChatResponse = chat(model='llama3', messages=[{'role': 'user', 'content': "Ten restaurants to visit in Newcastle? Names only"}])
+    print(response.message.content)
+    return {"response": response.message.content}
