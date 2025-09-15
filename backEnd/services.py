cf1c6081a0b5f1d5f303c3c0656f6b2f627bfc4e
NathMiller <nathanmiller5304@gmail.com>
2025-09-15 14:25:19 +0100

    Added error handling to the AI response in case no response can be generated

--- backEnd/services.py
+++ backEnd/services.py
@@ -2,16 +2,22 @@ from ollama import chat, ChatResponse
 
 
 def improveEmail(text: str) -> str:
-    response: ChatResponse = chat(model='gemma:2b', 
-                                  messages=[
-                                      {
-                                          "role": "system",
-                                          "content": "Your only focus is to rewrite emails. Do not answer other questions"
-                                      },
-                                      {'role': 'user', 
-                                       'content': f"Make the following text into a professional email that is clear and concise: \n\n{text}"
-                                       }])
-    print(response.message.content)
 
-    displayResponse = response['message']['content']
-    return displayResponse
+    try: 
+        response: ChatResponse = chat(model='gemma:2b', 
+                                    messages=[
+                                        {
+                                            "role": "system",
+                                            "content": "Your only focus is to rewrite emails. Do not answer other questions"
+                                        },
+                                        {'role': 'user', 
+                                        'content': f"Make the following text into a professional email that is clear and concise: \n\n{text}"
+                                        }])
+        print(response.message.content)
+
+        displayResponse = response['message']['content']
+        return displayResponse
+
+    except Exception as e:
+        print(f"Error calling Ollama: {e}")
+        return "Failure to connect to Ollama."
